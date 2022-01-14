f35ee77474de75cafeab8243ba8485d08e4832cd
Sayedelmahdy <sayedahmed01093307397@gmail.com>
2022-01-14 15:35:50 +0200

    Add project files.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        /// 
+        Label label1 = new Label();
+        Label label2 = new Label();
+        Label label3 = new Label();
+        TextBox txt1 = new TextBox();
+        TextBox txt2 = new TextBox();
+        TextBox txt3 = new TextBox();
+        Button btn1 = new Button();
+        Button btn2 = new Button();
+        Button btn3 = new Button();
+        Button btn4 = new Button();
+        private void InitializeComponent()
+        {
+            
+
+           // label
+            label1.Location = new Point(60,20);
+            label1.Text = "number 1";
+            label1.Size = new Size(80,50);
+            label1.ForeColor = Color.Red;
+            label2.Location = new Point(260, 20);
+            label2.Text = "number 2";
+            label2.Size = new Size(80, 50);
+            label2.ForeColor = Color.Red;
+            label3.Location = new Point(460, 20);
+            label3.Text = "Result";
+            label3.Size = new Size(80, 50);
+            label3.ForeColor = Color.Red;
+
+            // textbox
+            txt1.Location = new Point(140, 20);
+            txt2.Location = new Point(340, 20);
+            txt3.Location = new Point(540, 20);
+
+            // button 
+            btn1.Location = new Point(60, 80);
+            btn1.Text = "Add";
+            btn1.Width = 80;
+            btn1.Click += new System.EventHandler(Add);
+            btn2.Location = new Point(160, 80);
+            btn2.Text = "Substract";
+            btn2.Width = 80;
+            btn2.Click += new System.EventHandler(sub);
+            btn3.Location = new Point(260, 80);
+            btn3.Text = "Multiply";
+            btn3.Width = 80;
+            btn3.Click += new System.EventHandler(multi);
+            btn4.Location = new Point(360, 80);
+            btn4.Text = "Divid";
+            btn4.Width = 80;
+            btn4.Click += new System.EventHandler(divid);
+
+
+
+
+            //form
+            this.Text = "Menu Demo";
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+
+            //Controls
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn4);
+        }
+
+        #endregion
+    }
+}
+
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Add (object sender , EventArgs e)
+        {
+            txt3.Text = Convert.ToString(Convert.ToInt32(txt1.Text) + Convert.ToInt32(txt2.Text));
+            this.Refresh();
+        }
+        private void sub(object sender, EventArgs e)
+        {
+            txt3.Text = Convert.ToString(Convert.ToInt32(txt1.Text) - Convert.ToInt32(txt2.Text));
+            this.Refresh();
+        }
+        private void multi(object sender, EventArgs e)
+        {
+            txt3.Text = Convert.ToString(Convert.ToInt32(txt1.Text) * Convert.ToInt32(txt2.Text));
+            this.Refresh();
+        }
+        private void divid(object sender, EventArgs e)
+        {
+            txt3.Text = Convert.ToString(Convert.ToInt32(txt1.Text) / Convert.ToInt32(txt2.Text));
+            this.Refresh();
+        }
+    }
+}
